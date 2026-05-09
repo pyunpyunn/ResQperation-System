1eb19303fe8ce3c9809e70ab63dd929ef9ad1ba9
Ekayy10 <erecajoy10@gmail.com>
2026-05-09 12:40:56 +0800

    Fix: Downgrade Expo SDK to match Play Store version and fix execution policy

--- backend/tests/Feature/ExampleTest.php
+++ backend/tests/Feature/ExampleTest.php
@@ -14,6 +14,8 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertRedirect('/login');
+        $response
+            ->assertOk()
+            ->assertJsonPath('type', 'ResQperation Laravel API');
     }
 }
